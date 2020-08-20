b2ac86b4db3eb48842e8f9b01c28b62e9bd99c2a
이태헌(Taehun Lee)/AI개발팀/SKP <thlee0102@sk.com>
2020-08-20 23:36:22 +0900

    Leek IntToRoman

---
+++ src/test/java/com/thlee/algorithms/leek/IntToRomanTest.java
@@ -0,0 +1,28 @@
+package com.thlee.algorithms.leek;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntToRomanTest {
+
+    @ParameterizedTest
+    @MethodSource("arguments")
+    void intToRoman(int num, String expected) {
+        IntToRoman intToRoman = new IntToRoman();
+        String result = intToRoman.intToRoman(num);
+        assertEquals(expected, result);
+    }
+
+    private static Stream<Arguments> arguments() {
+        return Stream.of(
+            Arguments.of(3, "III"),
+            Arguments.of(1994, "MCMXCIV")
+        );
+    }
+}
